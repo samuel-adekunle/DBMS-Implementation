175d3031229c13b784512028e8f9edc206876af1
Samuel Adekunle <me@samuel-adekunle.software>
2022-02-14 07:08:09 +0000

    Defined interfaces and made execution plan.
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

---
+++ Source/AdvancedDB2022Coursework1.cpp
@@ -0,0 +1,26 @@
+#include "AdvancedDB2022Coursework1.hpp"
+
+const Relation *DBMSImplementationForMarks::hashJoin(const Relation *probeSide, const Relation *buildSide) {
+    // TODO
+    return {};
+}
+
+const Relation *DBMSImplementationForMarks::sortRelation(const Relation *relation) {
+    // TODO
+    return {};
+}
+
+const Relation *DBMSImplementationForMarks::sortMergeJoin(const Relation *leftSide, const Relation *rightSide) {
+    // TODO
+    return {};
+}
+
+const Relation *DBMSImplementationForMarks::select(const Relation *input, const int threshold) {
+    // TODO
+    return {};
+}
+
+long DBMSImplementationForMarks::sum(const Relation *input) {
+    // TODO
+    return 0;
+}
--- Source/AdvancedDB2022Coursework1.hpp
+++ Source/AdvancedDB2022Coursework1.hpp
@@ -7,21 +7,26 @@
 #include <variant>
 #include <vector>
 // YOU MAY NOT ADD ANY OTHER INCLUDES!!!
-using AttributeValue = std::variant<long, double, char const*>;
+using AttributeValue = std::variant<long, double, char const *>;
 using Tuple = std::vector<AttributeValue>;
 using Relation = std::vector<Tuple>;
 
 /**
  * 0 islong, 1 is double, 2 is a c-string
  */
-inline size_t getAttributeValueType(AttributeValue const& value) { return value.index(); }
-inline long getLongValue(AttributeValue const& value) { return std::get<long>(value); }
-inline double getdoubleValue(AttributeValue const& value) { return std::get<double>(value); }
-inline char const* getStringValue(AttributeValue const& value) {
-  return std::get<char const*>(value);
+inline size_t getAttributeValueType(AttributeValue const &value) { return value.index(); }
+
+inline long getLongValue(AttributeValue const &value) { return std::get<long>(value); }
+
+inline double getdoubleValue(AttributeValue const &value) { return std::get<double>(value); }
+
+inline char const *getStringValue(AttributeValue const &value) {
+    return std::get<char const *>(value);
 }
-inline size_t getNumberOfValuesInTuple(Tuple const& t) { return t.size(); }
-inline size_t getNumberOfTuplesInRelation(Relation const& t) { return t.size(); }
+
+inline size_t getNumberOfValuesInTuple(Tuple const &t) { return t.size(); }
+
+inline size_t getNumberOfTuplesInRelation(Relation const &t) { return t.size(); }
 
 /**
  * DBMS shall implement the following query in the query function:
@@ -30,37 +35,58 @@ inline size_t getNumberOfTuplesInRelation(Relation const& t) { return t.size(); 
  * large1.a = large2.a and large2.a = small.a and large1.b + large2.b + small.b
  * > 9;
  */
-class DBMSImplementationForMarks { // you may edit anything inside this class
-                                   // but nothing else
-  Relation large1;
+class DBMSImplementationForMarks {
+    // Pointers to relations
+    // Lifetimes are guaranteed to be longer than DBMS implementation so no need to garbage collect
+    const Relation *large1, *large2, *small;
+
+    // Implements hash join algorithm
+    // Smaller relation should be used as the buildSide
+    const Relation *hashJoin(const Relation *probeSide, const Relation *buildSide);
+
+    // Returns a sorted relation
+    const Relation *sortRelation(const Relation *relation);
+
+    // Implements sort-merge join algorithm
+    const Relation *sortMergeJoin(const Relation *leftSide, const Relation *rightSide);
+
+    // Selects tuples where sum of b attribute values is greater than the threshold
+    const Relation *select(const Relation *input, int threshold);
+
+    // Returns sum of product of c attribute values
+    long sum(const Relation *input);
 
 public:
-  void loadData(Relation const* large1,
-                Relation const* large2, // NOLINT(bugprone-easily-swappable-parameters)
-                Relation const* small)  // NOLINT(bugprone-easily-swappable-parameters)
-  {
-    this->large1 = *large1; // you can copy the relation or just keep a pointer
+    void loadData(Relation const *l1,
+                  Relation const *l2,
+                  Relation const *s) {
 
-    // here is some example code for the exatraction of values feel free to
-    // access the data any way you like (without changing the function
-    // signature, of course)
-    if(getAttributeValueType(this->large1[0][0]) == 0) {
-      long exampleLoadedValue = getLongValue(this->large1[0][0]);
+        // save pointers to tables
+        large1 = sortRelation(l1);
+        large2 = l2;
+        small = s;
     }
-    // You should add your implementation here...
-  }
 
-  long runQuery(long threshold = 9) {
-    auto sum = 0L;
-    // You should add your implementation here...
-    return sum;
-  }
+    long runQuery(long threshold = 9) {
+        const Relation *buffer1 = hashJoin(large2, small);
+        const Relation *buffer2 = sortRelation(buffer1);
+        const Relation *buffer3 = sortMergeJoin(buffer1, large1);
+        const Relation *buffer4 = select(buffer2, threshold);
+        long result = sum(buffer4);
+
+        // clean up buffers on heap
+        delete buffer1;
+        delete buffer2;
+        delete buffer3;
+        delete buffer4;
+
+        return result;
+    }
 };
 
 class DBMSImplementationForCompetition : public DBMSImplementationForMarks {
 public:
-  static constexpr char const* teamName =
-      nullptr; // set this to your team name if you mean to compete
+    static constexpr char const *teamName = nullptr;
 };
 
 #endif /* ADVANCEDDB2022COURSEWORK1_H */
