e887fc012e9a158ab9963d13cc4558976c2c37ec
Samuel Adekunle <me@samuel-adekunle.software>
2022-02-15 09:08:14 +0000

    Saving current state.
    Implementing sort-merge join and adding Myriam to repo
    
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- Source/AdvancedDB2022Coursework1.cpp
+++ Source/AdvancedDB2022Coursework1.cpp
@@ -1,26 +1,51 @@
 #include "AdvancedDB2022Coursework1.hpp"
 
-const Relation *DBMSImplementationForMarks::hashJoin(const Relation *probeSide, const Relation *buildSide) {
-    // TODO
-    return {};
+// **HELPER FUNCTIONS TO CHECK CONSTRAINTS ON WEAKLY TYPED ATTRIBUTE VALUES**
+
+bool DBMSImplementationForMarks::equals(const AttributeValue &left, const AttributeValue &right) {
+    return false;
 }
 
-const Relation *DBMSImplementationForMarks::sortRelation(const Relation *relation) {
-    // TODO
-    return {};
+bool DBMSImplementationForMarks::lessThan(const AttributeValue &left, const AttributeValue &right) {
+    return false;
 }
 
-const Relation *DBMSImplementationForMarks::sortMergeJoin(const Relation *leftSide, const Relation *rightSide) {
+// **MAIN QUERY FUNCTIONS**
+
+const Relation *DBMSImplementationForMarks::hashJoin(const Relation *probeSide, const Relation *buildSide,
+                                                     const size_t attributeIndex) {
+    if (probeSide == nullptr || buildSide == nullptr) { return nullptr; }
     // TODO
-    return {};
+    return new Relation;
 }
 
-const Relation *DBMSImplementationForMarks::select(const Relation *input, const int threshold) {
+const Relation *DBMSImplementationForMarks::sortRelation(const Relation *relation, const size_t attributeIndex) {
+    if (relation == nullptr) { return nullptr; }
     // TODO
-    return {};
+    return new Relation;
 }
 
-long DBMSImplementationForMarks::sum(const Relation *input) {
+const Relation *DBMSImplementationForMarks::sortMergeJoin(const Relation *leftSide, const Relation *rightSide,
+                                                          const size_t attributeIndex) {
+    if (leftSide == nullptr || rightSide == nullptr) { return nullptr; }
+    size_t leftIndex = 0, rightIndex = 0;
+    while (leftIndex < leftSide->size() && rightIndex < rightSide->size()) {
+        Tuple leftTuple = leftSide->at(leftIndex);
+        Tuple rightTuple = rightSide->at(rightIndex);
+    }
+    return new Relation;
+}
+
+const Relation *
+DBMSImplementationForMarks::select(const Relation *input, const int threshold, const size_t attributeIndex) {
+    if (input == nullptr) { return nullptr; }
+    // TODO
+    return new Relation;
+}
+
+// Returns sumOfProduct of product of c attribute values
+long DBMSImplementationForMarks::sumOfProduct(const Relation *input, size_t attributeIndex) {
+    if (input == nullptr) { return 0; }
     // TODO
     return 0;
 }
--- Source/AdvancedDB2022Coursework1.hpp
+++ Source/AdvancedDB2022Coursework1.hpp
@@ -12,7 +12,7 @@ using Tuple = std::vector<AttributeValue>;
 using Relation = std::vector<Tuple>;
 
 /**
- * 0 islong, 1 is double, 2 is a c-string
+ * 0 is long, 1 is double, 2 is a c-string
  */
 inline size_t getAttributeValueType(AttributeValue const &value) { return value.index(); }
 
@@ -31,30 +31,48 @@ inline size_t getNumberOfTuplesInRelation(Relation const &t) { return t.size(); 
 /**
  * DBMS shall implement the following query in the query function:
  *
- * select sum(large1.c * large2.c * small.c) from large1, large2, small where
+ * select sumOfProduct(large1.c * large2.c * small.c) from large1, large2, small where
  * large1.a = large2.a and large2.a = small.a and large1.b + large2.b + small.b
  * > 9;
  */
 class DBMSImplementationForMarks {
-    // Pointers to relations
+    // **POINTERS TO RELATIONS**
     // Lifetimes are guaranteed to be longer than DBMS implementation so no need to garbage collect
+
     const Relation *large1, *large2, *small;
+
+    // **QUERY PARAMETERS**
+
+    const size_t sortAttributeIndex = 0; // a
+    const size_t joinAttributeIndex = 0; // a
+    const size_t selectAttributeIndex = 1; // b
+    const size_t sumAttributeIndex = 2; // c
+
+    // **HELPER FUNCTIONS TO CHECK CONSTRAINTS ON WEAKLY TYPED ATTRIBUTE VALUES**
+
+    // Checks equality constraint on two weakly typed attribute values
+    inline bool equals(const AttributeValue &left, const AttributeValue &right);
+
+    // Checks less than constraint on two weakly typed attribute values
+    inline bool lessThan(const AttributeValue &left, const AttributeValue &right);
+
+    // **MAIN QUERY FUNCTIONS**
 
     // Implements hash join algorithm
     // Smaller relation should be used as the buildSide
-    const Relation *hashJoin(const Relation *probeSide, const Relation *buildSide);
+    const Relation *hashJoin(const Relation *probeSide, const Relation *buildSide, size_t attributeIndex);
 
     // Returns a sorted relation
-    const Relation *sortRelation(const Relation *relation);
+    const Relation *sortRelation(const Relation *relation, size_t attributeIndex);
 
     // Implements sort-merge join algorithm
-    const Relation *sortMergeJoin(const Relation *leftSide, const Relation *rightSide);
+    const Relation *sortMergeJoin(const Relation *leftSide, const Relation *rightSide, size_t attributeIndex);
 
     // Selects tuples where sum of b attribute values is greater than the threshold
-    const Relation *select(const Relation *input, int threshold);
+    const Relation *select(const Relation *input, int threshold, size_t attributeIndex);
 
     // Returns sum of product of c attribute values
-    long sum(const Relation *input);
+    long sumOfProduct(const Relation *input, size_t attributeIndex);
 
 public:
     void loadData(Relation const *l1,
@@ -62,17 +80,17 @@ public:
                   Relation const *s) {
 
         // save pointers to tables
-        large1 = sortRelation(l1);
+        large1 = sortRelation(l1, sortAttributeIndex);
         large2 = l2;
         small = s;
     }
 
     long runQuery(long threshold = 9) {
-        const Relation *buffer1 = hashJoin(large2, small);
-        const Relation *buffer2 = sortRelation(buffer1);
-        const Relation *buffer3 = sortMergeJoin(buffer1, large1);
-        const Relation *buffer4 = select(buffer2, threshold);
-        long result = sum(buffer4);
+        const Relation *buffer1 = hashJoin(large2, small, joinAttributeIndex);
+        const Relation *buffer2 = sortRelation(buffer1, sortAttributeIndex);
+        const Relation *buffer3 = sortMergeJoin(large1, buffer2, joinAttributeIndex);
+        const Relation *buffer4 = select(buffer2, threshold, selectAttributeIndex);
+        long result = sumOfProduct(buffer4, sumAttributeIndex);
 
         // clean up buffers on heap
         delete buffer1;
